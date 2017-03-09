921e43e4f7f54c660e0918ddcfb396bbcfeccbbc
unknown <gsingh@miraclesoft.com>
2017-03-09 11:59:53 -0500

    commit

---
+++ node_modules/newrelic/newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['microservices-app'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '47370fccb23be2de41d035abe0acc8237d51c4d5',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
